60c17b59ee06131fa4c501694d433426db50fa16
ne290149 <ne290149@senshu-u.jp>
2019-01-03 22:24:55 +0900

    管理者で会員登録と管理者登録できるようにした

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -18,10 +18,12 @@ class Admin::MembersController < Admin::Base
   end
 
   def create
-    @member = Member.new(member_params)
+    @member = Member.new(member_params2)
     #@member.assign_attributes(member_params)
+    puts "!!!!!!!!!!!!!admin/members/create"
+	puts @member.is_admin
     if @member.save
-      redirect_to controller: [:admin, @member], notice: "会員登録が完了しました"
+      redirect_to [:admin, @member], notice: "会員登録が完了しました"
     else
       render "edit"
     end
@@ -55,13 +57,13 @@ class Admin::MembersController < Admin::Base
 
   #管理者によるアカウント追加用
   private
-  def member_params
+  def member_params2
     attrs = [:user_id, :name, :place, :tel, :mail_address,:defpassword, :password_confirmation, :is_admin]
     params.require(:member).permit(attrs)
   end
   #管理者による会員のプロフィール情報編集用
   private
-  def member_params2
+  def member_params
     attrs = [:user_id, :name, :place, :tel, :mail_address,:defpassword, :password_confirmation]
     params.require(:member).permit(attrs)
   end

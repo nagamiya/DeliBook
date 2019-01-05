297cf96ae4806f7544e1997bc87f065efde2df69
ne290149 <ne290149@senshu-u.jp>
2019-01-05 21:28:44 +0900

    管理者で貸出一覧の検索をできるようにした（本の名前で検索）

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,15 @@
 class Rental < ActiveRecord::Base
 	belongs_to :member
 	belongs_to :book
+
+  class << self
+    def search(query)
+      rel = order("id")
+      if query.present?
+	rel = rel.joins(:book)
+        rel = rel.where("books.name LIKE ?", "%#{query}%")
+      end
+      rel
+    end
+  end
 end

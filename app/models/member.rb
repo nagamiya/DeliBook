663e18a5351e549b308a0a514ff36e769a6c064b
ne290149 <ne290149@senshu-u.jp>
2019-01-11 09:50:43 +0900

    Memberのバリデーション

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,23 +1,27 @@
 class Member < ActiveRecord::Base
+  include EmailAddressChecker
   has_many :reservations, dependent: :destroy
   has_many :rentals, dependent: :destroy
 
  validates :user_id, presence: true,
-    format: { with: /\A[A-Za-z]\w*\z/, allow_blank: true },
-    length: { minimum: 2, maximum: 20, allow_blank: true },
+    format: { with: /\A[A-Za-z0-9_]\w*\z/, allow_blank: true },
+    length: { minimum: 2, maximum: 16, allow_blank: true },
     uniqueness: true
   validates :name, presence: true,
     length: { maximum: 20 }
-  #住所
   validates :tel, presence: true,
     format: { with: /\A[0-9()-]+\z/, allow_blank: true},
     length: { maximum: 15, allow_blank: true },
     uniqueness: { case_sensitive: false }
-  validates :mail_address, presence: true,#:check_email
-    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-    uniqueness: { case_sensitive: false }
-  validates :password, presence: { on: :create },
+  validate :check_email
+  #validates :mail_address, presence: true,#:check_email
+   # format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+   # uniqueness: { case_sensitive: false }
+  validates :password, presence: true,
+    format: { with: /\A[A-Za-z0-9]+\z/, allow_blank: true },
+    length: { minimum: 6, maximum: 8 },
     confirmation: { allow_blank: true }
+
   attr_accessor :password, :password_confirmation
 
   def password=(val)
@@ -27,6 +31,13 @@ class Member < ActiveRecord::Base
 		puts "pass::" + self.hashed_password
 	end
 	@password = val
+  end
+
+  private
+  def check_email
+    if mail_address.present?
+      errors.add(:mail_address, :invalid) unless well_formed_as_email_address(mail_address)
+    end
   end
 
   class << self
--- db/seeds/development/members.rb
+++ db/seeds/development/members.rb
@@ -1,7 +1,7 @@
 user_ids = %w(nagamiyaid toosakaid emiyaid iriyaid giruid)
 names = %w(永宮舞帆 遠坂凛 衛宮士郎 イリヤスフィール ギルガメッシュ)
 places = %w(ウルク 冬木 冬木 冬木 ウルク)
-tels = %w(000 111 222 333 444)
+tels = %w(000-0000 111-1111 222-2222 333-3333 444-4444)
 mail_addresses = %w(nagamiya000 toosaka111 emiya222 iriya333 giru444)
 #passwords = %w(mahomaho rinrin sirosiro iriiri girugiru)
 0.upto(4) do |idx|
